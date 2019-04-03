6dc0746c55cd9a5be10d33680a86dfad8b32b6f9
siyuanren <siyuanre@usc.edu>
2019-04-03 18:32:18 +0800

    Create Hello_Python.py

---
+++ Hello_Python.py
@@ -0,0 +1,24 @@
+print("Hello Python")
+message="hello python"
+print(message)
+
+message= "hello world"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name ="ada"
+last_name = "lovelace"
+full_name =first_name + " " + last_name 
+print(full_name)
+print("hello, " + full_name.title() + "!")
+
+message= "hello, " + full_name.title()+ "!"
+print(message)
+
+message= "Siyuan wants to sleep"
+print(message)
+
